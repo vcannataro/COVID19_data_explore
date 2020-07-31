3b8178ef30bd6876dfb251c7a9f3b2fa620edf39
vcannataro <cannatarov@emmanuel.edu>
2020-07-31 10:40:10 -0400

    data update and NE plot

---
+++ analyses/update_useful_plots.R
@@ -0,0 +1,7 @@
+# update plots after pulling NYT github repo
+
+
+source("exploring_counties.R")
+source("analyses/cases_V_day_of_week.R")
+source("analyses/MA_data_plots.R")
+source("analyses/New_England_states.R")
